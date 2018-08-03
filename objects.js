bd5edfb3350670db8f96878856ec33cfd5565524
Lee Simon <shinoblee@gmail.com>
2018-08-03 11:11:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist(object, name, title){
   object.assign({}, object,{[name]: title});
   return object;
 }
+function removeFromPlaylist(object, name){
+  delete object.name;
+  return object
+}
